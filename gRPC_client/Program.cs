3acb32d225c09a430b644d5ea9edb3d7324b7689
Manuel E Mendez <emilio_mem@hotmail.com>
2022-05-12 11:33:33 -0400

    Add project files.

---
+++ gRPC_client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+
+
+
+
+using (var channel = GrpcChannel.ForAddress("https://localhost:7231"))
+{
+    var client = new Greeter.GreeterClient(channel);
+    var reply = await client.SayHelloAsync(new HelloRequest
+    {
+        Name = "Emilio"
+    });
+    Console.WriteLine(reply.Message);
+}
